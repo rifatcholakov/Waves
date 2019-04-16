252b217075bebe8de4937d45e8b2deb579cbb242
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-16 18:31:24 +0300

    Login users and creating tokens

--- server/server.js
+++ server/server.js
@@ -34,6 +34,23 @@ app.post('/api/users/register', (req, res) => {
     });
 });
 
+app.post('/api/users/login', (req, res) => {
+    User.findOne({ 'email': req.body.email }, (err, user) => {
+        if(!user) return res.json({ loginSuccess: false, message: 'We were unable to find user with this email!' });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({ loginSuccess: false, message: 'Incorrect password! Please try again.' });
+
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth', user.token).status(200).json({
+                    loginSuccess: true
+                });
+            });
+        });
+    });
+});
+
 const port = process.env.PORT || 3002;
 
 app.listen(port, () => {
