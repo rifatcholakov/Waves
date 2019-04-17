f73084efe93e08179022b11a5f54c9ebd9e8d363
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-17 14:53:36 +0300

    Header and footer

---
+++ client/src/routes.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Layout from './hoc/layout';
+import Home from './components/Home';
+
+
+const Routes = () => {
+  return(
+    <Layout>
+      <Switch>
+      <Route path="/" exact component={Home} />
+      </Switch>
+    </Layout>
+  )
+}
+
+
+export default Routes;
