0c23c95940bcfe4ce316d0830ea0d5fafbcc9344
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-19 22:57:57 +0300

    Shop checkboxes

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -4,11 +4,17 @@ import PageTop from '../utils/page_top';
 import { connect } from 'react-redux';
 import { getBrands, getWoods } from '../../actions/products_actions';
 
+import CollapseCheckbox from '../utils/collapseCheckbox';
+
 class Shop extends Component {
 
     componentDidMount() {
         this.props.dispatch(getBrands());
         this.props.dispatch(getWoods());
+    }
+
+    handleFilters = () => {
+        
     }
 
     render() {
@@ -21,7 +27,15 @@ class Shop extends Component {
                 <div className="container">
                     <div className="shop_wrapper">
                         <div className="left">
-                            left
+                            <CollapseCheckbox
+                                initState={true}
+                                title="Brands"
+                                list={products.brands}
+                                handleFilters={(filters)=> this.handleFilters(filters,'brand')}
+                            />
+
+
+
                         </div>
                         <div className="right">
                             right
