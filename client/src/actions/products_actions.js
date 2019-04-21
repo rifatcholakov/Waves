b8f36e0881974f252ad6a8d51a7a421b2cace988
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-21 21:59:26 +0300

    Manage brands component

--- client/src/actions/products_actions.js
+++ client/src/actions/products_actions.js
@@ -3,6 +3,7 @@ import {
     GET_PRODUCTS_BY_SELL,
     GET_PRODUCTS_BY_ARRIVAL,
     GET_BRANDS,
+    ADD_BRANDS,
     GET_WOODS,
     GET_PRODUCTS_TO_SHOP,
     ADD_PRODUCT,
@@ -92,6 +93,27 @@ export function getBrands() {
         payload: request
     }
 }
+
+export function addBrand(dataToSubmit, existingBrands) {
+    const request = axios.post(`${PRODUCT_SERVER}/brand`, dataToSubmit)
+                    .then(response => {
+                        let brands = [
+                            ...existingBrands,
+                            response.data.brand
+                        ];
+
+                        return {
+                            success: response.data.success,
+                            brands
+                        }
+                    });
+    
+    return {
+        type: ADD_BRAND,
+        payload: request
+    }
+}
+
 
 export function getWoods() {
     const request = axios.get(`${PRODUCT_SERVER}/woods`)
---
+++ client/src/components/User/Admin/manage_woods.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ManageWoods extends Component {
+    render() {
+            return (
+            <div>
+                woods
+            </div>
+        );
+    }
+}
+
+export default ManageWoods;
