d9bb1fcbd1f7bf2a9ce93ff08214217ddfb34ca2
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-21 13:36:58 +0300

    Adming - add products form

--- client/src/components/utils/Form/formfield.js
+++ client/src/components/utils/Form/formfield.js
@@ -23,6 +23,9 @@ const Formfield = ({ formdata, change, id }) => {
             case('input'):
                 formTemplate = (
                     <div className="formBlock">
+                        { formdata.showlabel ?
+                            <div className="label_inputs">{formdata.config.label}</div>
+                        :null}
                         <input 
                             {...formdata.config}
                             value={formdata.value}
