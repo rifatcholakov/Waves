38f35fcb0dee794340bfc56bfdde40a2e2d7ccd3
Rifat Cholakov <rifatcholakov@gmail.com>
2019-04-21 12:16:45 +0300

    Shop grids

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -10,6 +10,9 @@ import CollapseCheckbox from '../utils/collapseCheckbox';
 import CollapseRadio from '../utils/collapseRadio';
 
 import LoadmoreCards from './loadmoreCards';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import faBars from '@fortawesome/fontawesome-free-solid/faBars';
+import faTh from '@fortawesome/fontawesome-free-solid/faTh';
 
 class Shop extends Component {
 
@@ -89,6 +92,12 @@ class Shop extends Component {
         })
     }
 
+    handleGrid = () => {
+        this.setState({
+            grid: !this.state.grid ? 'grid_bars' : ''
+        })
+    }
+    
     render() {
         const products = this.props.products;
         return (
@@ -131,7 +140,18 @@ class Shop extends Component {
                         <div className="right">
                             <div className="shop_options">
                                 <div className="shop_grids clear">
-                                    grids
+                                    <div
+                                        className={`grid_btn ${this.state.grid ? '' : 'active'}`}
+                                        onClick={() => this.handleGrid()}
+                                    >
+                                        <FontAwesomeIcon icon={faTh} />
+                                    </div>
+                                    <div
+                                        className={`grid_btn ${!this.state.grid ? '' : 'active'}`}
+                                        onClick={() => this.handleGrid()}
+                                    >
+                                        <FontAwesomeIcon icon={faBars} />
+                                    </div>
                                 </div>
                             </div>
                             <div>
